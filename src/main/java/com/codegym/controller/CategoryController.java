6bc703cf89574c414ee36be75b1373d453293ba8
loiminh <loiho11082015@gmail.com>
2020-05-10 16:51:33 +0700

    add create category done

--- src/main/java/com/codegym/controller/CategoryController.java
+++ src/main/java/com/codegym/controller/CategoryController.java
@@ -8,27 +8,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.Optional;
 
 @Controller
 @RequestMapping("/category")
 public class CategoryController {
-    @Autowired
-    private BlogService blogService;
 
     @Autowired
     private CategoryService categoryService;
-
-    @ModelAttribute("blogs")
-    public Page<Blog> findAll(Pageable pageable){
-        return blogService.findAll(pageable);
-    }
 
     @GetMapping
     public ModelAndView showFormCategory(@RequestParam("search")Optional<String> search, Pageable pageable){
@@ -41,6 +32,20 @@ public class CategoryController {
         ModelAndView modelAndView = new ModelAndView("/category/list");
         modelAndView.addObject("category", categories);
         return modelAndView;
+    }
+
+    @GetMapping("/create")
+    public ModelAndView showCreateFormCategory(){
+        ModelAndView modelAndView = new ModelAndView("/category/create");
+        modelAndView.addObject("category", new Category());
+        return modelAndView;
+    }
+
+    @PostMapping("/create")
+    public String createCategory(@ModelAttribute("category") Category category, RedirectAttributes redirect){
+        categoryService.save(category);
+        redirect.addFlashAttribute("message", "create category successfully !");
+        return "redirect:/category";
     }
 
 }
